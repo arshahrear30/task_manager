f988eaa9d08bb317ae621cba33baa73cd54cecb9
arshahrear30 <arshahrear30@gmail.com>
2026-03-03 15:54:07 +0600

    add new task screen e kaz korci conseft 13 30 min

--- lib/ui/screens/add_new_task_screen.dart
+++ lib/ui/screens/add_new_task_screen.dart
@@ -10,6 +10,11 @@ class AddNewTaskScreen extends StatefulWidget {
 }
 
 class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
+  final TextEditingController _subjectTEController = TextEditingController();
+  final TextEditingController _descriptionTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _createTaskInProgress = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,50 +27,89 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
                 child: SingleChildScrollView(
                   child: Padding(
                     padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-
-                        const SizedBox(height: 32,),
-                        Text('Add New Task', style: Theme.of(context).textTheme.titleLarge,),
-                        const SizedBox(height: 32,),
-                
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            hintText: 'Subject',
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                      
+                          const SizedBox(height: 32,),
+                          Text('Add New Task', style: Theme
+                              .of(context)
+                              .textTheme
+                              .titleLarge,),
+                          const SizedBox(height: 32,),
+                      
+                          TextFormField(
+                            controller: _subjectTEController,
+                            decoration: const InputDecoration(
+                              hintText: 'Subject',
+                            ),
+                              validator: (String? value) {
+                                if (value?.trim().isEmpty ?? true) {
+                                  return 'Enter your subject';
+                                }
+                                return null;
+                              },
                           ),
-                        ),
+                      
+                      
+                          const SizedBox(height: 16,),
+                          TextFormField(
+                            controller: _descriptionTEController,
+                            maxLines: 8,
+                            decoration: const InputDecoration(
+                              hintText: 'Description',
+                            ),
 
-
-                        const SizedBox(height: 16,),
-                        TextFormField(
-                          maxLines: 8,
-                          decoration: const InputDecoration(
-                            hintText: 'Description',
-                          ),
-                        ),
-                        const SizedBox(height: 16,),
-                        SizedBox(
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.pop(context);
+                            validator: (String? value) {
+                              if (value?.trim().isEmpty ?? true) {
+                                return 'Enter your subject';
+                              }
+                              return null;
                             },
-                            child: const Icon(Icons.arrow_circle_right_outlined),
+
                           ),
-                
-                        ),
-                
-                
-                      ],
+                          const SizedBox(height: 16,),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: createTask,
+                              child: const Icon(
+                                  Icons.arrow_circle_right_outlined),
+                            ),
+
+                          ),
+
+                        ],
+                      ),
                     ),
-                  ), // Column
+                  ),
                 ),
-              ), // SingleChildScrollView
-            ) // Expanded
+              ),
+            )
           ],
-        ), // Column
-      ), // SafeArea
-    ); // Scaffold
+        ),
+      ),
+    );
   }
+
+  Future<void> createTask() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+
+    }
+
+
+
+
+  @override
+  void dispose() {
+    _subjectTEController.dispose();
+    _descriptionTEController.dispose();
+    super.dispose();
+  }
+
 }
